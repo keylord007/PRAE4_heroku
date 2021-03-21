acecce54fbfbc8fc0bf458f5026fddb8079d0ea8
keylord007 <keylordominguez@gmail.com>
2021-03-21 19:08:03 -0300

    style header, config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 var config = {
-    db_uri:"mongodb+srv://admink:key@praedb.drfcw.mongodb.net/praedb",
+    db_uri:"mongodb+srv://admink:key@praedb.drfcw.mongodb.net/praedb?retryWrites=true&w=majority",
     db_database: "praedb",
     db_secret: crypto,
 }
