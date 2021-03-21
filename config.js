8765bbb31d05f3a7d731092c64941bfebfc83ca8
keylord007 <keylordominguez@gmail.com>
2021-03-21 12:06:27 -0300

    Update name db

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 var config = {
-    db_uri:"mongodb+srv://admink:key@cluster0.sjtj8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    db_database: "myFirstDatabase",
+    db_uri:"mongodb+srv://admink:key@cluster0.sjtj8.mongodb.net/praedb",
+    db_database: "praedb",
     db_secret: crypto,
 }
 
