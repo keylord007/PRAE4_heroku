6d872a6d49e11e036be13d34a8c0865bde4f28a6
keylord007 <keylordominguez@gmail.com>
2021-03-24 00:39:13 -0300

    *por/

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json({ limit: '100mb' }));
 
 app.use(express.static(__dirname+'/dist'));
 
-app.get('*', function(req, res){
+app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/dist/index.html'));
   });
 //Routers
